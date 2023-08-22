25068eb512c3270eed9e740b8cd29f7f96633481
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-08-22 17:19:03 -0300

    Add CRUD

---
+++ src/main/java/com/example/springIntebras/SpringIntebrasApplication.java
@@ -0,0 +1,12 @@
+package com.example.springIntebras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntebrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntebrasApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/springIntebras/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.springIntebras.dao;
+
+import com.example.springIntebras.model.Person;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+}
